c07086825246ca304969bfe4edf9b5769a13a71f
Anthony Fan <anthonyfan@tencent.com>
2016-06-06 12:44:21 +0800

    创建项目。

---
+++ src/main/java/patentsniffer/App.java
@@ -0,0 +1,10 @@
+package patentsniffer;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("专利嗅探器启动...");
+        System.out.println("专利嗅探器关闭...");
+    }
+
+}
